93c50e240057404471aa162e2d571fb9f489a379
Nicolas Larenas <Nicolas Larenas@10.50.113.95>
2016-05-20 12:44:21 -0300

    Priemara página de inicio

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # testsurvey
+Es una aplicación por la cual los alumnos pueden decir según su punto de vista la dificultad de sus pruebas y en que puntos de la materia se sintieron más vulnerables. El fin de esta app es acercar esta información al profesor para que pueda evaluar futuras mejoras de su curso.
---
+++ inicio.php
@@ -0,0 +1,84 @@
+<html>
+<html>
+<head>
+<title>Proyecto GIK</title>
+</head>
+<body 
+
+<tr>
+
+		<td><h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		TEST SURVEY</h1></td>
+		
+		<td><h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		</h4>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		
+		<a href='inicio.php'>INICIO</a></td>
+
+		
+
+
+
+</body>
+</html>
+<?php
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software  Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ *
+ * @package local
+ * @subpackage testsurvey
+ * @copyright 2012-onwards Nicolás Larenas <nlarenas@alumnos.uai.cl>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+// Minimum for Moodle to work, the basic libraries
+require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
+// Parameter passed from the url.
+//$name = required_param('name', PARAM_TEXT);
+// Moodle pages require a context, that can be system, course or module (activity or resource)
+$context = context_system::instance();
+$PAGE->set_context($context);
+// Check that user is logued in the course.
+require_login();
+$PAGE->set_pagelayout('incourse');
+// Show the page header
+echo $OUTPUT->header();
+
+echo '<td><h1></h1>
+		<h1>&nbsp;&nbsp;
+		AYUDANOS A AYUDARTE
+				</h1></td>';
+echo '<html>
+		<body>
+			<table>	<td><center>
+				<td><form action="ver_proyecto.php" method="post" >
+		<input type="submit"   value="Crear encuesta" /></form></td><td></td>
+				
+				<td><form action="ver_proyecto.php" method="post" >
+		<input type="submit"   value="Resumen" /></form></td><td></td>
+				
+				<td><form action="ver_proyecto.php" method="post" >
+		<input type="submit"   value="Borar encuesta" /></form></td><td></td>
+				
+				
+
+				
+</table>
+</body>
+</html>';
+
+// Show the page footer
+echo $OUTPUT->footer();
+?>
